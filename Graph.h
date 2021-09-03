23ccb867bf04d9017f21788b412a6045ac43f460
lsx412 <1140576746@qq.com>
2021-09-03 15:55:44 +0800

    2021/09/03 周五 15:55:44.35 remove stdio

--- Graph.h
+++ Graph.h
@@ -1,12 +1,10 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
-#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #define MAXVEX 20
 #define INFINITY 32768
 #define OK 1
-//#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
 typedef char VertexType; /* 顶点类型应由用户定义  */
@@ -50,27 +48,6 @@ Status CreatUDN(MyGraph& G)
 		G.arc[j][i] = G.arc[i][j];
 	}
 	return OK;
-}
-
-
-/*建立无向网图的邻接矩阵表示*/
-void CreateMGraph(MyGraph& G)
-{
-	int i, j, k, w;
-	printf("输入顶点数和边数:\n");
-	scanf("%d,%d", &G.numNodes, &G.numEdges); /* 输入顶点数和边数 */
-	for (i = 0; i < G.numNodes; i++) /* 读入顶点信息,建立顶点表 */
-		scanf(&G.vexs[i]);
-	for (i = 0; i < G.numNodes; i++)
-		for (j = 0; j < G.numNodes; j++)
-			G.arc[i][j] = INFINITY;	/* 邻接矩阵初始化 */
-	for (k = 0; k < G.numEdges; k++) /* 读入numEdges条边，建立邻接矩阵 */
-	{
-		printf("输入边(vi,vj)上的下标i，下标j和权w:\n");
-		scanf("%d,%d,%d", &i, &j, &w); /* 输入边(vi,vj)上的权w */
-		G.arc[i][j] = w;
-		G.arc[j][i] = G.arc[i][j]; /* 因为是无向图，矩阵对称 */
-	}
 }
 #endif // !_GRAPH_H_
 
